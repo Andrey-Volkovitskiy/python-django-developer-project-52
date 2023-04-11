817d97f76b4e7e0c99cb874c3d3191af951dba7d
Andrey <volk000@gmail.com>
2023-04-12 00:15:42 +0600

    PG succeessfuly connected to Django

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,11 +3,21 @@ from . import settings
 
 
 def index(request):
+    from django.db import connections
+    from django.db.utils import OperationalError
+    db_conn = connections['default']
+    try:
+        c = db_conn.cursor()
+    except OperationalError:
+        db_connected = False
+    else:
+        db_connected = True
     return render(
         request,
         'index.html',
         context={
             'who': "Andrey",
-            'secret_key': settings.SECRET_KEY
+            'secret_key': settings.SECRET_KEY,
+            'db_connected': db_connected
         }
     )
