e2127466763f2bf270aa892843d9f9eeda20d4da
Andrey <volk000@gmail.com>
2023-04-17 13:46:17 +0600

    Show list of users

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,11 +1,13 @@
 from django.views.generic.base import TemplateView
+from django.contrib.auth.models import User
 
 
-class IndexView(TemplateView):
+class ListView(TemplateView):
 
     template_name = "users/list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['who'] = 'User'
+        all_users = User.objects.all()
+        context['all_users'] = all_users
         return context
