ecfd33b6df46018e2a1eb24eea742ce6e91ac09b
Andrey <volk000@gmail.com>
2023-04-23 17:04:34 +0600

    Add UpdateUserPage untested

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -5,4 +5,6 @@ from task_manager.users import views
 urlpatterns = [
     path('', views.UserListView.as_view(), name='user-list'),
     path('create/', views.CreateUserView.as_view(), name='user-create'),
+    path('<int:pk>/update/', views.UpdateUserView.as_view(),
+         name='user-update'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from task_manager.users.forms import UserForm
@@ -15,3 +15,11 @@ class CreateUserView(SuccessMessageMixin, CreateView):
     template_name = "users/create.html"
     success_url = '/users/'  # TODO
     success_message = _("User successfully created")
+
+
+class UpdateUserView(SuccessMessageMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = "users/update.html"
+    success_url = '/users/'
+    success_message = _("User successfully updated")
