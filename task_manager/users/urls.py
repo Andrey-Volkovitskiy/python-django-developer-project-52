e40cb9bad7b08c1ec7ca02545664260ccf7561fa
Andrey <volk000@gmail.com>
2023-04-24 08:13:06 +0600

    Add UserDeletePage without tests

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,7 +4,9 @@ from task_manager.users import views
 
 urlpatterns = [
     path('', views.UserListView.as_view(), name='user-list'),
-    path('create/', views.CreateUserView.as_view(), name='user-create'),
-    path('<int:pk>/update/', views.UpdateUserView.as_view(),
+    path('create/', views.UserCreateView.as_view(), name='user-create'),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(),
          name='user-update'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(),
+         name='user-delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from task_manager.users.forms import UserForm
 
@@ -10,16 +11,24 @@ class UserListView(ListView):
     template_name = "users/list.html"
 
 
-class CreateUserView(SuccessMessageMixin, CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserForm
     template_name = "users/create.html"
-    success_url = '/users/'  # TODO
+    success_url = reverse_lazy("user-list")  # TODO
     success_message = _("User successfully created")
 
 
-class UpdateUserView(SuccessMessageMixin, UpdateView):
+class UserUpdateView(SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserForm
     template_name = "users/update.html"
-    success_url = '/users/'
+    success_url = reverse_lazy("user-list")
     success_message = _("User successfully updated")
+
+
+class UserDeleteView(SuccessMessageMixin, DeleteView):
+    model = User
+    fields = []
+    template_name = "users/delete.html"
+    success_url = reverse_lazy("user-list")
+    success_message = _("User successfully deleted")
