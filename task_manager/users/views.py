d2bd9db9856e304bcef1e890e76581a0301bac8c
Andrey <volk000@gmail.com>
2023-04-27 11:02:20 +0600

    Implement EditUser, DelUser permissions

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,13 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.views.generic import (ListView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import (UserPassesTestMixin,
+                                        LoginRequiredMixin)
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from task_manager.users.forms import UserForm
@@ -14,11 +21,42 @@ class UserListView(ListView):
 class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserForm
     template_name = "users/create.html"
-    success_url = reverse_lazy("login")  # TODO
+    success_url = reverse_lazy("login")
     success_message = _("User successfully created")
 
 
-class UserUpdateView(SuccessMessageMixin, UpdateView):
+class UserPermissionsMixin(UserPassesTestMixin):
+    def test_func(self):
+        subject_user_id = self.request.user.id
+        object_user_id = self.kwargs['pk']
+        if subject_user_id == object_user_id:
+            return True
+        else:
+            return False
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.add_message(
+                self.request,
+                messages.ERROR,
+                _("You do not have rights to change another user.")
+            )
+            return redirect(reverse_lazy('user-list'))
+
+        else:
+            messages.add_message(
+                self.request,
+                messages.ERROR,
+                _("You are not authorized! Please sign in.")
+            )
+            return redirect(reverse_lazy('login'))
+
+
+class UserUpdateView(
+            LoginRequiredMixin,
+            UserPermissionsMixin,
+            SuccessMessageMixin,
+            UpdateView):
     model = User
     form_class = UserForm
     template_name = "users/update.html"
@@ -26,7 +64,11 @@ class UserUpdateView(SuccessMessageMixin, UpdateView):
     success_message = _("User successfully updated")
 
 
-class UserDeleteView(SuccessMessageMixin, DeleteView):
+class UserDeleteView(
+            LoginRequiredMixin,
+            UserPermissionsMixin,
+            SuccessMessageMixin,
+            DeleteView):
     model = User
     fields = []
     template_name = "users/delete.html"
